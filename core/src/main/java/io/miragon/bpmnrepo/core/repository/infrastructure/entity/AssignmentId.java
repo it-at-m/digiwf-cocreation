8b5da6cd18e78f47348e312c976bf9b5a35e98eb
Dominik Horn <dominik.horn93@gmail.com>
2021-06-18 13:11:07 +0200

    chore: init nexus build

--- core/src/main/java/io/miragon/bpmnrepo/core/repository/infrastructure/entity/AssignmentId.java
+++ core/src/main/java/io/miragon/bpmnrepo/core/repository/infrastructure/entity/AssignmentId.java
@@ -1,16 +1,14 @@
 package io.miragon.bpmnrepo.core.repository.infrastructure.entity;
 
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import javax.persistence.Column;
 import javax.persistence.Embeddable;
 import java.io.Serializable;
 
 @Getter
+@Setter
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
