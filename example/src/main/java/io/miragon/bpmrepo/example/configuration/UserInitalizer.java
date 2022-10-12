0f50c97d92434dc25f6c0df54549e5801899b6bc
Lukas Mösle <lukas95m@yahoo.de>
2022-10-12 11:10:59 +0200

    fix: dev setup

--- example/src/main/java/io/miragon/bpmrepo/example/configuration/UserInitalizer.java
+++ example/src/main/java/io/miragon/bpmrepo/example/configuration/UserInitalizer.java
@@ -1,24 +1,21 @@
 package io.miragon.bpmrepo.example.configuration;
 
-import io.miragon.bpmrepo.core.user.domain.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
-
-import javax.annotation.PostConstruct;
 
 @Component
 @RequiredArgsConstructor
 public class UserInitalizer {
 
-    private final UserService userService;
-
-    @PostConstruct
-    public void init() {
-        try {
-            this.userService.createUser("demo.user");
-        } catch (final Exception error) {
-
-        }
-    }
+//    private final UserService userService;
+//
+//    @PostConstruct
+//    public void init() {
+//        try {
+//            this.userService.createUser("demo.user");
+//        } catch (final Exception error) {
+//
+//        }
+//    }
 
 }
